21377aa934f0cf24f25f97c85f40f87bc664e9fb
Gatsby <maduddles@gmail.com>
2021-11-14 22:30:49 -0800

    increased timeout

--- pages/404.js
+++ pages/404.js
@@ -8,14 +8,14 @@ const NotFound = () => {
   useEffect(() => {
     setTimeout(() => {
       router.push('/')
-    }, 6000)
+    }, 7000)
   }, [])
 
   return (
     <div className="not-found">
       <h1>404</h1>
       <h2>Oops! That page cannot be found :(</h2>
-      <p>Redirecting to <Link href="/"><a>Homepage</a></Link> now...</p>
+      <p>Redirecting to <Link href="/"><a>Homepage</a></Link> in few seconds...</p>
 
       <style jsx>{`
         .not-found {
