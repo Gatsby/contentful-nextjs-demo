ab0a68ad9942408b2abc198e403214dd472b6b19
Gatsby <maduddles@gmail.com>
2021-11-14 21:56:24 -0800

    fallback page style fix

--- components/Skeleton.js
+++ components/Skeleton.js
@@ -13,7 +13,7 @@ export default function Skeleton() {
             margin: 20px auto;
           }
           .skeleton > div {
-            background: #e0e0e0;
+            background: #bbb;
             border-radius: 4px;
             margin: 20px 0;
           }
